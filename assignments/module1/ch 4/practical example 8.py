4b569ce4c9c033b69968fdd5fe8fbb5b0979e103
Bhavesh <baroliyabhavesh@gmail.com>
2025-05-08 17:46:09 -0700

    Aadi

--- assignments/module1/ch 4/practical example 8.py
+++ assignments/module1/ch 4/practical example 8.py
@@ -1,12 +1,12 @@
 age=int(input("Enter your age :"))
-bg=input("Enter your blood group :")
+bg=input("Enter your blood group to donate it to AB+ :")
 w=int(input("Enter your weight:"))
 if age>=18:
     if w>=45 and w<=65:
         if bg=="AB+":
-            print("You are eligible for donation of blood to many people")
+            print("You are eligible and you can donate the blood to AB+ blood group people")
         else:
-            print("you are eligible but to a fewer section of people")
+            print("you are eligible but you cannot donate blood to AB+ blood group")
     else:
         print("you are not eligible for donation of blood")
 else:
