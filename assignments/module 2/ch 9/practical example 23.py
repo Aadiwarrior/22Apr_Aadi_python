eaa181bca50e8d1f4e97c5e7ac8cb3c4e74cdc08
mrcharan21 <charanguguloth13@gmail.com>
2025-06-26 17:31:43 +0530

    aadi

---
+++ assignments/module 2/ch 9/practical example 23.py
@@ -0,0 +1,7 @@
+import math
+sr=int(input("Enter the number you want to root: "))
+print(math.sqrt(sr))
+ru=float(input("Enter the number you want to round up: "))
+print(math.ceil(ru))
+rd=float(input("Enter the number you want to round down: "))
+print(math.floor(rd))
--- lab practice/billing.py
+++ lab practice/billing.py
@@ -1,27 +1,29 @@
+import pandas
+
 def billing():
     total=0
-    items=[]
+    items={}
 
     while True:
         name=input("Enter food name:")
-        price=float(input("Enter the pice of your food:"))
+        price=float(input("Enter the price of your food:"))
         qty=int(input("enter the quantity of your food:"))
 
         item_total=qty*price
         total += item_total
-        items.append((name,qty,price,item_total))
+        items[name]={
+            "price": price,
+            "quantity": qty,
+            "total": item_total
+        }
         more=input("Do you want more items (1 for yes and 2 for no):").lower()
         if more !="yes":
             break
 
     print("\n==========Bill Summary==========\n")
-    print("name     price       quantity        subtotal")
-
-    for item in items:
-            name,qty,price,item_total=item
-            
-            print(f"{name}      {price}     {qty}       {item_total}")
-    print("\n Grand Total \n",total)
+    x=pandas.DataFrame(items)
+    print(x)
+    print("\n Total Amount: Rs.", total)
 
 
 billing()
